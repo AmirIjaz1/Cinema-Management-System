fbfa92ef7007b0a18a9aa26d81178fb4147d246c
Amir <amir.ijaz119988@gmail.com>
2022-06-24 17:53:10 +0500

    CMS-Oracle DB

---
+++ src/main/java/com/mycompany/cms/Constants.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cms;
+
+/**
+ *
+ * @author AmirIjaz
+ */
+public class Constants {
+
+    public String CustomerUserEmail;
+    public String cinemaSelected;
+    public String movieSelected;
+
+    public String getCinemanSelected() {
+        return cinemaSelected;
+    }
+
+    public void setCinemanSelected(String value) {
+        
+        this.cinemaSelected = value;
+    }
+
+}
